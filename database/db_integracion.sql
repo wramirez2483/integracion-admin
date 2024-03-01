48c5414278035552983f0ef4c8a1cc6572439e70
SaPradilla <andress_garces@soy.sena.edu.co>
2024-03-01 10:26:19 -0500

    database change

--- database/db_integracion.sql
+++ database/db_integracion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 01-03-2024 a las 14:12:07
+-- Tiempo de generación: 01-03-2024 a las 15:25:01
 -- Versión del servidor: 8.2.0
 -- Versión de PHP: 8.2.13
 
@@ -49,26 +49,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(65) NOT NULL,
   `role` enum('admin','reader') NOT NULL,
   `tipe_id` enum('cc','ti','ce','pep','ppt') NOT NULL,
-  `num_id` int NOT NULL,
-  `password` varchar(30) NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `num_id` bigint NOT NULL,
   `date_created` timestamp NOT NULL,
   `date_updated` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `num_id` (`num_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `role`, `tipe_id`, `num_id`, `password`, `date_created`, `date_updated`) VALUES
-(1, 'prueba', 'correo@correo.com', 'admin', 'cc', 123456789, '$2y$10$rAjjfJ/let8j.ULgX87.hed', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'prueba2', 'correo@correo.com', 'reader', 'cc', 12345678, '$2y$10$wLEubccJ0lS6LFxBFVyUs.O', '2024-02-29 21:09:35', '0000-00-00 00:00:00'),
-(3, 'matt', 'matt@gmail.com', 'admin', 'ti', 45698712, '$2y$10$IHhgMKs6yD4nHdmRjWnrr.I', '2024-02-29 21:31:51', '0000-00-00 00:00:00'),
-(5, 'Matteo Agudelo López', 'matteoagulop06@gmail.com', 'admin', 'ti', 1089932508, '$2y$10$jp/7zhQTs636Mdva.g.d2.i', '2024-02-29 22:01:10', '0000-00-00 00:00:00'),
-(6, 'a', 'a@a.a', 'admin', 'cc', 123, '$2y$10$0g8367XFlDhR//zoW187fuj', '2024-02-29 22:44:33', '0000-00-00 00:00:00'),
-(7, 'quien', 'correo@corre3o.com', 'admin', 'cc', 1234567891, '$2y$10$0OYsm2W.aQF1XBX43uL/k.o', '2024-03-01 13:10:55', '0000-00-00 00:00:00'),
-(8, 'quien', 'correo@corre3do.com', 'admin', 'ppt', 2147483647, '$2y$10$liq9LXQkqoeZ.b.iMuhfP.y', '2024-03-01 13:11:49', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id`, `name`, `email`, `role`, `tipe_id`, `password`, `num_id`, `date_created`, `date_updated`) VALUES
+(22, 'Tulio', 'lolapt1985@gmail.com', 'admin', 'cc', '$2y$10$ocZTLgF8QOob109jsoMBW.WQs1Hk4VhuKtgf.wqZL85vqOK94Xg9q', 12345678910, '2024-03-01 15:23:59', '0000-00-00 00:00:00');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
