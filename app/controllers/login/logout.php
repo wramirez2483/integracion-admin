97234ce4aed05353c1297e2cc14af81278187e93
Matt06052005 <matteoagulop06@gmail.com>
2024-03-03 20:29:48 -0500

    registro_auditoria_login_logout

--- app/controllers/login/logout.php
+++ app/controllers/login/logout.php
@@ -1,10 +1,29 @@
 <?php
+require_once '../../config.php';
 
+// Iniciar la sesión
+session_start();
 
-    session_start();
-    session_destroy();
-    unset($_SESSION['logueado']);
-    header('Location: ../../../');
+// Verificar si se ha iniciado sesión antes de cerrarla
+if (isset($_SESSION['logueado']) && $_SESSION['logueado']) {
+    // Obtener el ID del usuario que está cerrando sesión
+    $user_id = $_SESSION['document'];
+    
+    // Definir el evento de cierre de sesión
+    $event = "logout";
+    
+    // Insertar un registro en la tabla de auditoría con el evento de cierre de sesión y el ID del usuario
+    $audit_sql = "INSERT INTO audit (id_users, events) VALUES (:id_users, :event)";
+    $audit_stmt = $pdo->prepare($audit_sql);
+    $audit_stmt->bindParam(':id_users', $user_id);
+    $audit_stmt->bindParam(':event', $event);
+    $audit_stmt->execute();
+}
 
+// Destruir la sesión
+session_destroy();
+
+// Redirigir al usuario a la página de inicio
+header('Location: ../../../');
 exit();
-
+?>
--- app/controllers/login/singin.php
+++ app/controllers/login/singin.php
@@ -1,9 +1,6 @@
 <?php
 require_once '../../config.php';
 
-// Mensaje de depuración
-// echo "Script de inicio de sesión alcanzado.";
-
 // Verifica si se ha enviado un formulario de inicio de sesión
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Almacena en variables los valores del formulario
@@ -11,11 +8,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tipe_id = $_POST['tipe_id'];
     $password = $_POST['password'];
 
-    // Mensaje de depuración
-    // echo "Datos del formulario recibidos: email=$email, password=$password";
-
     // Encuentra el usuario en la base de datos por email
-    $sql = "SELECT * FROM users WHERE  tipe_id = :tipe_id AND num_id = :num_id ";
+    $sql = "SELECT * FROM users WHERE tipe_id = :tipe_id AND num_id = :num_id";
 
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':num_id', $num_id);
@@ -23,35 +17,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute();
     $user = $stmt->fetch();
 
-    // Mensaje de depuración
-    // if(!$user) {
-    //     $_SESSION['error_message'] = 'Correo electrónico o contraseña incorrectos';
-    //     exit();
-    // }
     // Verifica si el usuario existe y la contraseña es correcta
     if ($user && password_verify($password, $user['password'])) {
         // Iniciar sesión
         $_SESSION['document'] = $num_id;
         $_SESSION['logueado'] = true;
+        $event = "singin";
 
-        // Mensaje de depuración
-        echo "Usuario autenticado correctamente.";
+        // Guardar registro en la tabla de auditoría con evento "signIn"
+        $audit_sql = "INSERT INTO audit (id_users, events) VALUES (:id_users, :event)";
+        $audit_stmt = $pdo->prepare($audit_sql);
+        $audit_stmt->bindParam(':id_users', $num_id);
+        $audit_stmt->bindParam(':event', $event); // Utilizando una consulta preparada también para $event
+        $audit_stmt->execute();
 
         // Redirigir al usuario a la página de inicio
         header('Location: ../../../views/inicio.php');
         exit();
 
     } else {
-
         // Las credenciales son incorrectas, redirigir de vuelta al formulario de inicio de sesión
         header('Location: /app-integracion');
-        // var_dump(password_verify($password, $user['password']));
-        // var_dump($password);
-        // var_dump($user);
-
-        $_SESSION['error_message'] = 'Información de usuairo inválida';
+        $_SESSION['error_message'] = 'Información de usuario inválida';
         exit();
-        
     }
 } else {
     // Si no se reciben datos por POST, redirigir al usuario de vuelta al formulario de inicio de sesión
